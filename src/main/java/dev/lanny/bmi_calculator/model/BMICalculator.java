9463c6b83e2c5bc0902854f872880fe9f6f57ee2
LannyRivero <lannyrivero288@gmail.com>
2025-02-09 11:58:07 +0100

    feat: Created test BMICalculatorTes, but it fails due to the method not being implemented

--- src/main/java/dev/lanny/bmi_calculator/model/BMICalculator.java
+++ src/main/java/dev/lanny/bmi_calculator/model/BMICalculator.java
@@ -2,4 +2,21 @@ package dev.lanny.bmi_calculator.model;
 
 public class BMICalculator {
 
+    public String getBMICategory(int i) {
+     
+        throw new UnsupportedOperationException("Unimplemented method 'getBMICategory'");
+    }
+
+    public Object calculateBMI(int i, int j) {
+   
+        throw new UnsupportedOperationException("Unimplemented method 'calculateBMI'");
+    }
+
+    public double calculateBMI(int i, double d) {
+    
+        throw new UnsupportedOperationException("Unimplemented method 'calculateBMI'");
+    }
+
+    
 }
+
