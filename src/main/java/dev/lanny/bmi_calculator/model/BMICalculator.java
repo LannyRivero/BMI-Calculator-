778fc3b93a4e51d6c99b939cb1d617a86f215fb3
LannyRivero <lannyrivero288@gmail.com>
2025-02-09 12:14:31 +0100

    Refactor: Use BMICategory enum to simplify getBMICategory method

--- src/main/java/dev/lanny/bmi_calculator/model/BMICalculator.java
+++ src/main/java/dev/lanny/bmi_calculator/model/BMICalculator.java
@@ -1,32 +1,16 @@
 package dev.lanny.bmi_calculator.model;
 
-public class BMICalculator {
 
-    public double calculateBMI(double weight, double height) {
-        
-        if (weight <= 0 || height <= 0) {
-            throw new IllegalArgumentException("Weight and height must be greater than zero");
+    public class BMICalculator {
+        public double calculateBMI(double weight, double height) {
+            if (weight <= 0 || height <= 0) {
+                throw new IllegalArgumentException("El peso y la altura deben ser mayores que cero.");
+            }
+            return weight / Math.pow(height, 2);
         }
-        return weight / Math.pow(height, 2);
-    }
-
-    public String getBMICategory(double bmi) {
-        if (bmi < 16) {
-            return "Delgadez severa";
-        } else if (bmi >= 16 && bmi < 17) {
-            return "Delgadez moderada";
-        } else if (bmi >= 17 && bmi < 18.5) {
-            return "Delgadez leve";
-        } else if (bmi >= 18.5 && bmi < 25) {
-            return "Peso Normal";
-        } else if (bmi >= 25 && bmi < 30) {
-            return "Sobrepeso";
-        } else if (bmi >= 30 && bmi < 35) {
-            return "Obesidad tipo I";
-        } else if (bmi >= 35 && bmi < 40) {
-            return "Obesidad tipo II";
-        } else {
-            return "Obesidad tipo III";
+    
+        public String getBMICategory(double bmi) {
+            return BMICategory.getCategory(bmi);
         }
     }
-}
+
