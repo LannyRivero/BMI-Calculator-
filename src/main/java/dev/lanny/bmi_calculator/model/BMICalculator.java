b495a33f36e9799f52d89436c82ace399ee3603f
LannyRivero <lannyrivero288@gmail.com>
2025-02-09 12:04:43 +0100

    feat: Implemented method calculateBMI and getBMICategory  in BMICalculator, test  passed successfully

--- src/main/java/dev/lanny/bmi_calculator/model/BMICalculator.java
+++ src/main/java/dev/lanny/bmi_calculator/model/BMICalculator.java
@@ -2,21 +2,31 @@ package dev.lanny.bmi_calculator.model;
 
 public class BMICalculator {
 
-    public String getBMICategory(int i) {
-     
-        throw new UnsupportedOperationException("Unimplemented method 'getBMICategory'");
+    public double calculateBMI(double weight, double height) {
+        
+        if (weight <= 0 || height <= 0) {
+            throw new IllegalArgumentException("Weight and height must be greater than zero");
+        }
+        return weight / Math.pow(height, 2);
     }
 
-    public Object calculateBMI(int i, int j) {
-   
-        throw new UnsupportedOperationException("Unimplemented method 'calculateBMI'");
+    public String getBMICategory(double bmi) {
+        if (bmi < 16) {
+            return "Delgadez severa";
+        } else if (bmi >= 16 && bmi < 17) {
+            return "Delgadez moderada";
+        } else if (bmi >= 17 && bmi < 18.5) {
+            return "Delgadez leve";
+        } else if (bmi >= 18.5 && bmi < 25) {
+            return "Peso Normal";
+        } else if (bmi >= 25 && bmi < 30) {
+            return "Sobrepeso";
+        } else if (bmi >= 30 && bmi < 35) {
+            return "Obesidad tipo I";
+        } else if (bmi >= 35 && bmi < 40) {
+            return "Obesidad tipo II";
+        } else {
+            return "Obesidad tipo III";
+        }
     }
-
-    public double calculateBMI(int i, double d) {
-    
-        throw new UnsupportedOperationException("Unimplemented method 'calculateBMI'");
-    }
-
-    
 }
-
